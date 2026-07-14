871bba1b6c911a57921dba64ca6a81acdb5a4e94
Leonardo Poblete <cbi2253009373@izt.uam.mx>
2026-07-13 23:28:46 -0600

    Ingresamos Pila.java que es interface

---
+++ src/Pila.java
@@ -0,0 +1,8 @@
+public interface Pila<E> extends Iterable<E> {
+    void apilar(E elemento);
+    E desapilar();
+    E consultarCima();
+    boolean esVacia();
+    int numElementos();
+    void limpiar();
+}
